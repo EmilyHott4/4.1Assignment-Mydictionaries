23968938369f45a370b6f676daa0e184f1517f91
Johnny <Johnny@test.com>
2022-09-08 12:14:15 -0500

    finished exercise

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -10,7 +10,23 @@ print()
 print('*****  start section 1 - print dictionary ********')
 print()
 
+print(phonebook)
+print(type(phonebook)) #type is a function in python that allows us to know what type of function you are dealing with (list or dictionary)
+# a key error means that value is not in your dictionary
+phone = phonebook['Chris']
 
+print(phone)
+
+print(len(phonebook)) #know the length of dictionary
+
+#mydictionary = dict(m=8, n=9) #another way to creat a dictionary through 'dict' function and give them a key value pair
+#m and n are string
+#8 and 9 are numbers
+mydict = {} #creates an empty dictionary
+print(mydict)
+
+
+#print(mydictionary)
 
 
 
@@ -19,12 +35,19 @@ print('*****  end section 1 ********')
 print()
 
 
-'''
+
 
 
 print()
-print('*****  start section 2 - search dictionary ********')
+print('*****  start section 2 - search dictionary ********') #search a dictionary
 print()
+
+name = 'Chris'
+
+if name in phonebook:
+    print(phonebook[name])  #you give the key, it gives back the value
+else:
+    print (name, 'not found')
 
 
 
@@ -46,6 +69,10 @@ print()
 print('*****  start section 3 - edit/append dictionary ********')
 print()
 
+print(phonebook)
+phonebook['Chris'] = '555-0123' #updates the dictionary with new number for chris
+phonebook['Joe'] = '555 - 4444' #adds Joe to the dictionary with a key valued pair
+print(phonebook)
 
 
 
@@ -63,6 +90,9 @@ print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
+#del phonebook['Chris'] #deletes chris from dictionary
+#if the key does not exist in dictionary you will get an error
+#print(phonebook)
 
 
 
@@ -78,6 +108,20 @@ print()
 print()
 print('*****  start section 5 - iterate through keys, values, items ********')
 print()
+
+#use for loop to iterate one by one
+for k in phonebook: #'k' used to be 'key'
+    print(k) #prints the names of the phonebook #iterates through the keys, its the default option
+    print(phonebook[k]) #key is just a variable you named, can change it to anything like 'k'
+
+for value in phonebook.values():#.values allows you to cycle through the values
+    print(value)
+    
+#for k,v in phonebook.items():
+    #print('key:', k 'value: ' , v)
+
+for tuple in phonebook.items(): #immutable = can't change mutable means you can change them
+    print(tuple)
 
 
 
@@ -96,6 +140,12 @@ print()
 print('*****  start section 6 - using get and clear ********')
 print()
 
+phone = phonebook.get("Chris,","key not found")
+print(phone)
+
+#phonebook.clear()
+#print(phonebook)
+
 
 
 
@@ -111,7 +161,10 @@ print()
 print('*****  start section 7 - using pop method ********')
 print()
 
+#remove = phonebook.pop('Chris','key not found')
 
+#print(remove)
+#print(phonebook)
 
 
 
@@ -126,6 +179,12 @@ print()
 print('*****  start section 8 - using popitem ********')
 print()
 
+#picks random key value pair
+#a = phonebook.popitem() #don't need to give it key cause picks an element
+#always picks the last element
+
+#print(a)
+#print(phonebook)
 
 
 
@@ -141,7 +200,16 @@ print()
 print('*****  start section 9 - using random and converting to list ********')
 print()
 
+list_of_keys = list(phonebook)
+print(list_of_keys) #when you use a list on dictionary it creates a list with just the keys
 
+random_key = random.choice(list_of_keys)
+print(random_key)
+
+print(phonebook[random_key])
+
+print(phonebook[random.choice(list(phonebook))])
+#exactly the same thing as above
 
 
 
@@ -149,8 +217,7 @@ print()
 print('*****  end section 9 ********')
 print()
 
-
-'''
+#random.choice will pick random element from list
 
 
 
